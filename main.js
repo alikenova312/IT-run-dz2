8fcb0000e8173959ebe2166ade70902aa32799c9
alikenova312 <alikenova312@icloud.com>
2022-07-30 14:10:21 +0600

    dz2

---
+++ main.js
@@ -0,0 +1,38 @@
+// let a = '34'
+// a = Number(34)
+// console.log(a)
+
+
+// let b = 29.92
+// console.log(Math.ceil(b))
+// console.log(Math.floor(b))
+// console.log(Math.round(b))
+// console.log(Math.trunc(b))
+
+// console.log(Math.ceil(Math.random() * 10))
+// console.log(Math.ceil(Math.random() *30) +70)
+// console.log(Math.ceil(Math.random() *200) +300)
+
+// let a = 'firStMonTh'
+// console.log(a.toUpperCase())
+// console.log(a.toLowerCase())
+
+// let a = 'Hello MF'
+// console.log(a[0])
+// console.log(a[1])
+// console.log(a[a.length -1])
+// console.log(a[a.length -2])
+
+// let b = 'Hello world'
+// console.log(b.length)
+
+// let a = 'arab boys'
+// console.log(a.includes('b'))
+// console.log(a.includes('boy'))
+// console.log(a.includes('arab', 6))
+// console.log(a.includes('boys', 3))
+// console.log(a.startsWith('a'))
+// console.log(a.endsWith('h'))
+
+// let c = 'arab boys'
+// console.log(c.toUpperCase().startsWith('arab'.toUpperCase()))
